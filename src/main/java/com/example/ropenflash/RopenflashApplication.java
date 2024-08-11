2f3b16b0c2ecd7aa8ceac06ea0b7df264fb35a78
Rishab Gupta <ropenflash@Rishabs-MacBook-Air.local>
2024-08-12 00:50:53 +0530

    initial commit

---
+++ src/main/java/com/example/ropenflash/RopenflashApplication.java
@@ -0,0 +1,13 @@
+package com.example.ropenflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopenflashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopenflashApplication.class, args);
+	}
+
+}
